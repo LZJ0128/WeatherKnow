822d68ae8c431d7bc718869508fcc1ac2636d826
lzj942 <2851728779@qq.com>
2016-02-26 18:30:15 +0800

    增加日期转化方法

--- app/src/main/java/com/lzj/weatherknow/helper/ConstantHelper.java
+++ app/src/main/java/com/lzj/weatherknow/helper/ConstantHelper.java
@@ -1,5 +1,7 @@
 package com.lzj.weatherknow.helper;
 
+import java.util.Calendar;
+
 /**
  * Created by Administrator on 2/16 0016.
  * 常量帮助类
@@ -21,5 +23,49 @@ public class ConstantHelper {
         String a = "https://api.heweather.com/x3/weather?cityid=";
         String b = "&key=a5b8008680104b2ab6923cfbdb8b2c9e";
         return a.concat(cityId).concat(b);
+    }
+
+    /**
+     * 星期几
+     * @param position
+     * @return
+     */
+    public static String getWeekday(int position){
+        Calendar c = Calendar.getInstance();
+        int weekday = c.get(Calendar.DAY_OF_WEEK) + position + 1;
+        String dayOfWeek = "";
+        switch (weekday){
+            case 1:
+            case 8:
+                dayOfWeek = "星期日";
+                break;
+            case 2:
+            case 9:
+                dayOfWeek = "星期一";
+                break;
+            case 3:
+            case 10:
+                dayOfWeek = "星期二";
+                break;
+            case 4:
+            case 11:
+                dayOfWeek = "星期三";
+                break;
+            case 5:
+            case 12:
+                dayOfWeek = "星期四";
+                break;
+            case 6:
+            case 13:
+                dayOfWeek = "星期五";
+                break;
+            case 7:
+            case 14:
+                dayOfWeek = "星期六";
+                break;
+            default:
+                break;
+        }
+        return dayOfWeek;
     }
 }
